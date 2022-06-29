41b45220e052dd5c2b5193ec80f058c457e065f6
pardonmyfrench <34434265+pardonmyfrench@users.noreply.github.com>
2022-06-29 17:37:03 -0400

    Update BookForm.vue
    Commit 1-4

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -2,6 +2,17 @@
   <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
     <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+    <label for="finishedReading">Finished Reading
+      <input v-model="bookData.finishedReading" type="checkbox" name="finished" id="finished" placeholder="Finished Reading">
+    </label>
+    </div>
+    <label for="borrowed">
+        <input v-model="bookData.ownership" type="radio" name="ownership" value="borrowed" id="borrowed">borrowed</label>
+      </label>
+    <label for="bought">
+        <input v-model="bookData.ownership" type="radio" name="ownership" value="bought" id="bought">bought</label>
+      </label>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
