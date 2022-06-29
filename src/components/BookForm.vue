4d8b705219d421239e09c48140437023482383f8
pardonmyfrench <34434265+pardonmyfrench@users.noreply.github.com>
2022-06-29 17:42:22 -0400

    Update BookForm.vue
    1-7

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,13 +1,13 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <div>
     <label for="finishedReading">Finished Reading</label>
       <input v-model="bookData.finishedReading" type="checkbox" name="finished" id="finishedReading">
     </div>
         <input v-model="bookData.ownership" type="radio" name="ownership" value="borrowed" id="borrowed">
-        <label for="bought">borrowed</label>
+        <label for="borrowed">borrowed</label>
         <input v-model="bookData.ownership" type="radio" name="ownership" value="bought" id="bought">
        <label for="bought">bought</label>
     <button type="submit" name="button">Add Book</button>
@@ -29,8 +29,8 @@ export default {
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
